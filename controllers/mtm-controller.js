43a7bf9a2826701c5903254a65e69e386f2f2c75
Kedar Bhumkar <kedar.bhumkar@gmail.com>
2020-11-01 17:30:15 -0600

    Add model

--- controllers/mtm-controller.js
+++ controllers/mtm-controller.js
@@ -1,30 +1,40 @@
-const uuid = require('uuid/v4');
-const { validationResult } = require('express-validator');
+const uuid = require("uuid/v4");
+const { validationResult } = require("express-validator");
 
-const HttpError = require('../models/http-error');
-const Mtm = require('../models/mtm');
+const HttpError = require("../models/http-error");
+const Mtm = require("../models/mtm");
 
-const getTest =  (req, res, next) => {
-  res.status(200).json({ 'Success': 'getTest' });
+const getTest = (req, res, next) => {
+  res.status(200).json({ Success: "getTest" });
 };
 
-const getActiveMTMcases =  (req, res, next) => {
-  console.log('Inside getActiveMTMcases')
-  returnResponse(req, res)
- 
+const getActiveMTMcases = (req, res, next) => {
+  console.log("Inside getActiveMTMcases");
+  //returnResponse(req, res)
+  payload = req.body
+  let activeCases = payload.map((p,index) => {
+    let res = {...Mtm.mtmEligibilityResponse}
+    console.log(index)
+    Math.floor(Math.random() * 10) > 5 ? res.caseID=uuid() : res.caseID=""
+    return res;
+  });
+  console.log("resData = " + activeCases);
+  res.status(201).json({ activeCases });
 };
 
-const postStatusUpdate =  (req, res, next) => {
-  console.log('Inside postStatusUpdate')
-  returnResponse(req, res)
+const postStatusUpdate = (req, res, next) => {
+  console.log("Inside postStatusUpdate");
+  returnResponse(req, res);
 };
 
-
-function returnResponse(req, res){
-  payload = req.body  
-  let  resData = payload.map(p=>({'caseId': p.caseId, 'status': Math.floor(Math.random() * 10) > 5?'success':'error'}))
-  console.log('resData = ' + resData)  
-  res.status(201).json({ resData})
+function returnResponse(req, res) {
+  payload = req.body;
+  let resData = payload.map((p) => ({
+    caseId: p.caseId,
+    status: Math.floor(Math.random() * 10) > 5 ? "success" : "error",
+  }));
+  console.log("resData = " + resData);
+  res.status(201).json({ resData });
 }
 
 exports.getTest = getTest;
---
+++ models/mtm.js
@@ -0,0 +1,73 @@
+const mtmEligibilityResponse = {
+
+	"clinicalProgramName": "MTM",
+
+	"identificationDate": "20200410",
+
+	"caseID": "MTM-12345",
+
+	"branding": "F",
+
+	"cycleYear": "2020",
+
+	"status": "Enrollment",
+
+	"closingDate": "20200610",
+
+	"workedBy": "Pathway",
+
+	"reworkReason": "string",
+
+	"fulfillmentId": "string",
+
+	"identifiers": {
+
+		"carrierId": "ACUMAEC",
+
+		"accountId": "ACUMAECCOMM",
+
+		"groupId": "ACUMAECCOMM",
+
+		"memberId": "109157252",
+
+		"medicareId": "3T87KF5VT71",
+
+		"contractId": "CUMAE",
+
+		"pbp": "358"
+
+	},
+
+	"memberDemographics": {
+
+		"memberFirstName": "PATRICIA",
+
+		"memberLastName": "NWABUEZE",
+
+		"memberDOB": "19901130",
+
+		"memberAddress1": "17 MILL ST",
+
+		"memberAddress2": "APT 506",
+
+		"memberCity": "ARLINGTON",
+
+		"memberState": "IL",
+
+		"memberZip": "2476",
+
+		"patientGender": "F"
+
+	},
+
+	"eligibility": {
+
+		"effectiveDate": "20200101",
+
+		"termDate": "20391231"
+
+	}
+
+}
+
+exports.mtmEligibilityResponse = mtmEligibilityResponse;
